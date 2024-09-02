213fa50b0a43441ed7c205bba664b041303a52c5
BiaTofano <beatriz.t.lemos@gmail.com>
2024-09-02 10:52:47 -0300

    Add files via upload

---
+++ exercício 3 lista 1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    int a;
+
+    printf("Digite seu número:");
+    scanf("%d", &a);
+
+
+    while (a<100){
+        if((a%2==0)&& (a%3==0)){
+            printf("%d: Pim Pam\n", a);
+
+        }
+        else{
+            if(a%2==0){
+                printf("%d: Pim\n", a);
+            }
+            else{
+                if(a%3==0){
+                    printf("%d: Pam\n", a);
+                }
+                else{
+                    printf("%d : %d\n", a, a);
+                }
+            }
+        }
+
+        a++;
+    }
+return 0;
+}
